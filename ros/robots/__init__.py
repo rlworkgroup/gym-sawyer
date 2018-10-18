ae27f221c88385c274afab88e9562af34ff9d4e7
Utkarsh Patel <utkarshjp7@gmail.com>
2018-10-17 22:24:29 -0700

    Added mujoco and ros envs for sawyer from garage repo

---
+++ ros/robots/__init__.py
@@ -0,0 +1,3 @@
+from garage.contrib.ros.robots.sawyer import Sawyer
+
+__all__ = ["Sawyer"]
---
+++ ros/worlds/__init__.py
@@ -0,0 +1,4 @@
+from garage.contrib.ros.worlds.block_world import BlockWorld
+from garage.contrib.ros.worlds.empty_world import EmptyWorld
+
+__all__ = ["BlockWorld", "EmptyWorld"]
