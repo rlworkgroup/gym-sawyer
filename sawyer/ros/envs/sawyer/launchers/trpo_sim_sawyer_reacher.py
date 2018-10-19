4a5f30f18410087f878fb117184333c2990288d4
Utkarsh Patel <utkarshjp7@gmail.com>
2018-10-19 11:39:49 -0700

    Added sawyer ros files from garage repo

--- sawyer/ros/envs/sawyer/launchers/trpo_sim_sawyer_reacher.py
+++ sawyer/ros/envs/sawyer/launchers/trpo_sim_sawyer_reacher.py
@@ -6,12 +6,12 @@ import moveit_commander
 import numpy as np
 import rospy
 
-from sawyer.garage.baselines import LinearFeatureBaseline
-from sawyer.ros.envs.sawyer import ReacherEnv
-from sawyer.garage.misc.instrument import run_experiment
-from sawyer.garage.theano.algos import TRPO
-from sawyer.garage.theano.envs import TheanoEnv
-from sawyer.garage.theano.policies import GaussianMLPPolicy
+from garage.baselines import LinearFeatureBaseline
+from garage.contrib.ros.envs.sawyer import ReacherEnv
+from garage.envs.util import spec
+from garage.misc.instrument import run_experiment
+from garage.theano.algos import TRPO
+from garage.theano.policies import GaussianMLPPolicy
 
 INITIAL_ROBOT_JOINT_POS = {
     'right_j0': -0.140923828125,
@@ -33,19 +33,18 @@ def run_task(*_):
 
     rospy.init_node('trpo_sim_sawyer_reacher_exp', anonymous=True)
 
-    env = TheanoEnv(
-        ReacherEnv(
-            initial_goal,
-            initial_joint_pos=INITIAL_ROBOT_JOINT_POS,
-            simulated=True))
+    env = ReacherEnv(
+        initial_goal,
+        initial_joint_pos=INITIAL_ROBOT_JOINT_POS,
+        simulated=True)
 
     rospy.on_shutdown(env.shutdown)
 
     env.initialize()
 
-    policy = GaussianMLPPolicy(env_spec=env.spec, hidden_sizes=(32, 32))
+    policy = GaussianMLPPolicy(env_spec=spec(env), hidden_sizes=(32, 32))
 
-    baseline = LinearFeatureBaseline(env_spec=env.spec)
+    baseline = LinearFeatureBaseline(env_spec=spec(env))
 
     algo = TRPO(
         env=env,
